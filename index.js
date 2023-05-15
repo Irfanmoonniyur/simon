28ccceeaf8d62fa92f6206177f22ddafcc3789b6
Irfanmoonniyur <109480063+Irfanmoonniyur@users.noreply.github.com>
2023-05-15 23:20:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+var buttonColours = ["red", "blue", "green", "yellow" ];
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+
+// second part
+$(".btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+    
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+
+    animatePress(userChosenColour);
+  
+    
+});
+
+
+// first part
+
+function nextSequence(){
+var randomNumber=Math.floor(Math.random()*4);
+
+var randomChosenColour=buttonColours[randomNumber];
+gamePattern.push(randomChosenColour);
+
+
+
+  
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+ playSound(randomChosenColour);
+  
+
+
+
+  
+}
+
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+
+}
+
+function animatePress(currentColour){
+    // give animation and remove after 100ms
+    $("#" + currentColour).addClass("pressed");
+
+
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+      }, 100);
+}
